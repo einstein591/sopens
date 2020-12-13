3168ef41934e4f2593c8423de7993afc05610240
einstein591 <75708951+einstein591@users.noreply.github.com>
2020-12-13 14:25:36 -0600

    Create FanficUtil.js

---
+++ FanficUtil.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         FanficUtil
+// @namespace    https://greasyfork.org/en/users/705154-ben-jackson
+// @version      5.1
+// @description  Adds navigation bar to multi-chapter stories in Fanfiction.net.
+// @author       sopens
+// @match        https://www.fanfiction.net/s/*
+// @grant        none
+// ==/UserScript==
+
+function getStoryUrl() {
+  
+}
+
+function getLocationHref() {
+  return window.location.href;
+}
