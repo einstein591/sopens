22c79d59a063e14f8d6b5ebf7ec72e51c6ce7560
einstein591 <75708951+einstein591@users.noreply.github.com>
2020-12-15 15:34:04 -0600

    Update FanficUtil.js

--- FanficUtil.js
+++ FanficUtil.js
@@ -1,17 +1,52 @@
-// ==UserScript==
-// @name         FanficUtil
-// @namespace    https://greasyfork.org/en/users/705154-ben-jackson
-// @version      5.1
-// @description  Adds navigation bar to multi-chapter stories in Fanfiction.net.
-// @author       sopens
-// @match        https://www.fanfiction.net/s/*
-// @grant        none
-// ==/UserScript==
+// FanficUtil
 
-function getStoryUrl() {
+function Person(url, chapter) {
+  this.baseUrl = url.split("/").slice(0, 3).join("/");
+  this.storyId = url.split("/")[4];
+  this.chapter = chapter;
+  this.storyName = url.split("/")[6];
+  this.baseStoryUrl = [baseUrl, "s"].join("/");
+  this.baseReviewUrl = [baseUrl, "r"].join("/");
   
-}
+  // Methods ...
 
-function getLocationHref() {
-  return window.location.href;
+  this.getPrevChapterUrl = function() {
+    if (storyName == null) {
+     return [baseStoryUrl, storyId, chapter - 1].join("/");
+    } else {
+     return [baseStoryUrl, storyId, chapter - 1, storyName].join("/");
+    }
+  }
+  
+  this.getCurrentChapterUrl = function() {
+    if (storyName == null) {
+     return [baseStoryUrl, storyId, chapter].join("/");
+    } else {
+     return [baseStoryUrl, storyId, chapter, storyName].join("/");
+    }
+  }
+  
+  this.getNextChapterUrl = function() {
+    if (storyName == null) {
+     return [baseStoryUrl, storyId, chapter + 1].join("/");
+    } else {
+     return [baseStoryUrl, storyId, chapter + 1, storyName].join("/");
+    }
+  }
+  
+  this.getStoryReviewUrl = function() {
+    return [baseUrl, "r", storyId].join("/");
+  }
+  
+  this.getPrevChapterReviewsUrl = function() {
+    return [getStoryReviewUrl(), chapter - 1, "1"].join("/");
+  }
+  
+  this.getCurrentChapterReviewsUrl = function() {
+    return [getStoryReviewUrl(), chapter, "1"].join("/");
+  }
+  
+  this.getNextChapterReviewsUrl = function() {
+    return [getStoryReviewUrl(), chapter + 1, "1"].join("/");
+  }
 }
